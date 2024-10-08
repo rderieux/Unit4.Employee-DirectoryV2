508d70ffdf586c16231fa3533ed3d1318e79aea2
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-08 13:33:08 -0600

    Add the application middleware to route to the api employees router

--- server.js
+++ server.js
@@ -6,26 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-const employees = require("./employees");
-
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
-
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
-});
+app.use("/employees", require("./api/employees"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
