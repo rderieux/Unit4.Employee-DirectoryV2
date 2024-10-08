9fcecf0c4bfe0ee11fb3f223ea7a27a685cc66e8
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-08 13:34:00 -0600

    Rename app. to router. in the api router

--- api/employees.js
+++ api/employees.js
@@ -4,16 +4,16 @@ module.exports = router;
 
 const employees = require("../data/employees");
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json(employees);
 });
 
-app.get("/random", (req, res) => {
+router.get("/random", (req, res) => {
   const i = Math.floor(Math.random() * employees.length);
   res.json(employees[i]);
 });
 
-app.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
   const employee = employees.find((e) => e.id === +id);
   if (employee) {
