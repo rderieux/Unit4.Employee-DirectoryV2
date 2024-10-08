c6bced84bc1b21c299a11b32139bc4896056852d
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-08 13:21:17 -0600

    Add error handling middleware

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something broke on our end.");
+});
+
 app.listen(PORT, () => {
   `Listening on port ${PORT}...`;
 });
